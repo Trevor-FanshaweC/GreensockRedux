7deeefc7285defe11a24efebbccff61c1f8c7697
tvanrys <tvanrys@fanshaweonline.ca>
2020-04-09 15:53:07 -0400

    done

--- js/main.js
+++ js/main.js
@@ -2,5 +2,18 @@
 (() => {
   console.log('fired!');
 
-  gsap.to(".box", { scaleX: 0.5, scaleY: 1.5, rotation: 26, x: 250, y: 100, duration: 1.5, ease: "bounce.out" });
+  function fireAnim() {
+    gsap.to(".box", { scaleX: 0.5, scaleY: 1.5, rotation: 26, x: 250, y: 100, duration: 1.5, ease: "bounce.out", onComplete: doSomethingElse, onCompleteParams: ["arg1", "or a house name, as an example"] });
+  }
+
+  function doSomethingElse(one, two) {
+    console.log('hello!', one, two);
+    gsap.to(".box", { scaleX: 0.5, scaleY: 0.5, rotation: 0, x: 150, y: 30, duration: 0.5, onComplete: doAThirdThing });
+  }
+
+  function doAThirdThing() {
+    console.log("now we're getting silly");
+  }
+
+  fireAnim();
 })();
