d38407bb0db8b3c16280bc9621897dc09d5a0cb4
tvanrys <tvanrys@fanshaweonline.ca>
2020-04-09 15:28:05 -0400

    finished with basic tween

--- js/main.js
+++ js/main.js
@@ -2,14 +2,5 @@
 (() => {
   console.log('fired!');
 
-  let person  = {
-    haircolor: "red",
-    eyes: "blue",
-    weight: "too much",
-    height: "180cm",
-
-    walk: () => { console.log('move my legs one at a time to walk') }
-  };
-
-  debugger;    
+  gsap.to(".box", { scaleX: 0.5, scaleY: 1.5, rotation: 26, x: 250, y: 100, duration: 1.5, ease: "bounce.out" });
 })();
