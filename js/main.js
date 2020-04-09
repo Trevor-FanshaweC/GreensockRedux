ae2aea31fca00349068e305827f301fca97be879
tvanrys <tvanrys@fanshaweonline.ca>
2020-04-09 13:33:56 -0400

    simple object explainer

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 // we'll wrap this in a module at the end
-console.log('fired!');
+(() => {
+  console.log('fired!');
+
+  let person  = {
+    haircolor: "red",
+    eyes: "blue",
+    weight: "too much",
+    height: "180cm",
+
+    walk: () => { console.log('move my legs one at a time to walk') }
+  };
+
+  debugger;    
+})();
